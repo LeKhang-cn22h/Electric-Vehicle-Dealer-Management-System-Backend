027ebbbc5727da3e82790374d22d00f776fe0d60
LeKhang-cn22h <chube02082017@gmail.com>
2025-11-06 21:53:01 +0700

    KAN-107 create customer service

--- apps/customer-service/test/app.e2e-spec.ts
+++ apps/customer-service/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { SalesModule } from './../src/sales.module';
+import { CustomerServiceModule } from './../src/customer-service.module';
 
-describe('SalesController (e2e)', () => {
+describe('CustomerServiceController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [SalesModule],
+      imports: [CustomerServiceModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
