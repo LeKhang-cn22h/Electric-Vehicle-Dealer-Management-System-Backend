60f148707fa497103997d3f6c3f2431a0cdf7f0e
hohuynhnhu <hhnhu2003@gmail.com>
2025-11-13 04:23:04 +0700

    fix module gateway

--- apps/ev-dealer-api-gateway/src/ev-dealer-api-gateway.module.ts
+++ apps/ev-dealer-api-gateway/src/ev-dealer-api-gateway.module.ts
@@ -5,7 +5,7 @@ import { GatewayAuthController } from './routes/gateway-auth.controller';
 import { GatewayUsersController } from './routes/gateway-users.controller';
 import { GatewayDealerCoordinationController } from './routes/gateway-dealer-coordination.controller';
 // import { GatewayBillingController } from './routes/gateway-billing.controller';
-
+import { GatewayEvmStaffCoordinationController } from './routes/gateway-evm-staff-coordination.controller';
 @Module({
   imports: [HttpModule.register({ timeout: 8000 })],
   providers: [ServiceClients],
@@ -14,6 +14,7 @@ import { GatewayDealerCoordinationController } from './routes/gateway-dealer-coo
     GatewayUsersController,
     GatewayDealerCoordinationController,
     // GatewayBillingController,
+    GatewayEvmStaffCoordinationController,
   ],
 })
 export class EvDealerApiGatewayModule {}
