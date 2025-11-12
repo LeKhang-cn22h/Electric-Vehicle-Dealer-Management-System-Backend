657454d9a685ac27f378124d599b791ceed461fb
hohuynhnhu <hhnhu2003@gmail.com>
2025-11-13 04:19:58 +0700

    fix main

--- apps/dealer-service-coordination/src/main.ts
+++ apps/dealer-service-coordination/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { DealerCoordinationModule } from './dealer-coordination.module';
+import { EvmStaffCoordinationModule } from './dealer-coordination.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(DealerCoordinationModule);
+  const app = await NestFactory.create(EvmStaffCoordinationModule);
   app.enableCors();
   await app.listen(3001);
   console.log('Dealer Service Coordination running on port 3001');
