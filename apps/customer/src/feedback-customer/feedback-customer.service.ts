62000d9b14f26cd75e63ad6cb9061d06db2ef1c1
LeKhang-cn22h <chube02082017@gmail.com>
2025-11-18 19:46:23 +0700

    WIP: save temporary changes before switching branch

--- apps/customer/src/feedback-customer/feedback-customer.service.ts
+++ apps/customer/src/feedback-customer/feedback-customer.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { SupabaseService } from '../supabase/supabase.service';
 @Injectable()
-export class FeedbackCustomerService {}
+export class FeedbackCustomerService {
+  private supabase;
+
+  constructor(private supabaseService: SupabaseService) {
+    this.supabase = this.supabaseService.getClient();
+  }
+}
