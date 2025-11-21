3c3808dce018fe3652d0ddeaf6c4bc2759b36548
LeKhang-cn22h <chube02082017@gmail.com>
2025-11-22 06:29:30 +0700

    fix jsocn error with service vehicle

--- apps/vehicle/src/appointments/test/app.e2e-spec.ts
+++ apps/vehicle/src/appointments/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { Vehicle/appointmentsModule } from './../src/vehicle/appointments.module';
+import request from 'supertest';
+import { AppointmentsModule } from '../../appointments/src/appointments.module';
 
-describe('Vehicle/appointmentsController (e2e)', () => {
+describe('appointmentsController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [Vehicle/appointmentsModule],
+      imports: [AppointmentsModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -16,9 +16,6 @@ describe('Vehicle/appointmentsController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 });
--- apps/vehicle/src/vehicle-new/src/vehicle-new.module.ts
+++ apps/vehicle/src/vehicle-new/src/vehicle-new.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { vehicleNewController } from './vehicle-new.controller';
 import { vehicleNewService } from './vehicle-new.service';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
-  imports: [],
+  imports: [RabbitMQModule],
   controllers: [vehicleNewController],
   providers: [vehicleNewService],
 })
